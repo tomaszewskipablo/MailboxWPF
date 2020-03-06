b3f7eb77c4ca4fdeb350adfee423ed7bec488215
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-03-06 12:39:45 +0100

    Emails list change if e-mail name is clicked

--- mailboxWPF/MainWindow.xaml.cs
+++ mailboxWPF/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace mailboxWPF
         {
             InitializeComponent();
         }
+
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListViewItem1.Content = "Get new iPhone";
+            ListViewItem2.Content = "New gift for you";
+            ListViewItem3.Content = "Volkswagen up!";
+            ListViewItem4.Content = "new movies in Cinema";
+            ListViewItem5.Content = "We have gift for you";
+            ListViewItem6.Content = "new movies in Cinema";
+            ListViewItem7.Content = "We have gift for you";
+            ListViewItem8.Content = "We have gift for you";
+            ListViewItem9.Content = "new movies in Cinema";
+            ListViewItem10.Content = "We have gift for you";
+            ListViewItem11.Content = "Post";
+            ListViewItem12.Content = "peyment reminder";
+        }
+
+
+        private void mail2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListViewItem1.Content = "Project replacemnt";
+            ListViewItem2.Content = "New appointemnt";
+            ListViewItem3.Content = "Appointment";
+            ListViewItem4.Content = "Emergency meeting";
+            ListViewItem5.Content = "Travell";
+            ListViewItem6.Content = "Appointment";
+            ListViewItem7.Content = "Comming students";
+            ListViewItem8.Content = "Money";
+            ListViewItem9.Content = "";
+            ListViewItem10.Content = "";
+            ListViewItem11.Content = "";
+            ListViewItem12.Content = "";
+        }
     }
 }
