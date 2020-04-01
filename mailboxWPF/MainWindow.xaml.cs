d5161af88f318482ffbacbe6caf4ac4206013d4a
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-01 23:08:19 +0200

    open dialog box if add atchement is clecked

--- mailboxWPF/MainWindow.xaml.cs
+++ mailboxWPF/MainWindow.xaml.cs
@@ -143,9 +143,6 @@ namespace mailboxWPF
                     deletedToListView(this.user2);
                 }
             }
-
-
-            //if(currentFolder=Folder.inbox)
         }
         private void mail1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
--- mailboxWPF/SendMessageWindow.xaml.cs
+++ mailboxWPF/SendMessageWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using Microsoft.Win32;
+
+
 namespace mailboxWPF
 {
     /// <summary>
@@ -26,13 +29,34 @@ namespace mailboxWPF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(subject.Text.Length > 0 && recipient.Text.Length > 0)
+            if (subject.Text.Length > 0 && recipient.Text.Length > 0)
             {
-               ;
+                ;
             }
             else
             {
                 MessageBox.Show("Fill recipient and subject fields to send the message", "Error");
+            }
+        }
+
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
             }
         }
     }
