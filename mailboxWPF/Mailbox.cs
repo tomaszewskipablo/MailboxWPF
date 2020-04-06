14754e81b3c2fd99677df25255d9122452530faa
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-06 18:50:51 +0200

    add new email messages for second user

--- mailboxWPF/Mailbox.cs
+++ mailboxWPF/Mailbox.cs
@@ -14,7 +14,7 @@ namespace mailboxWPF
         public List<Mail> sent;
         public List<Mail> deleted;
 
-        
+
 
         public Mailbox(string name)
         {
@@ -26,8 +26,8 @@ namespace mailboxWPF
         }
         public void AddMail(Mail mail, Folder folder)
         {
-            if(Folder.inbox==folder)
-             inbox.Add(mail);
+            if (Folder.inbox == folder)
+                inbox.Add(mail);
             if (Folder.spam == folder)
                 spam.Add(mail);
             if (Folder.sent == folder)
@@ -37,34 +37,68 @@ namespace mailboxWPF
         }
         public void LoadEmails()
         {
-            Mail mail = new Mail("Changes", "pawel.tomaszewski@gmail.com", "from@gmail.com", "There was many changes. Thanks for your time");
-            AddMail(mail, Folder.inbox);
-            mail = new Mail("Test mail", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
-            AddMail(mail, Folder.inbox);
-            mail = new Mail("Arsenal back home", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
-            AddMail(mail, Folder.inbox);
-            mail = new Mail("Injury", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
-            AddMail(mail, Folder.inbox);
-            mail = new Mail("new phone number", "pawel.tomaszewski@gmail.com", "from@gmail.com", "There was many changes. Thanks for your time");
-            AddMail(mail, Folder.inbox);
-            mail = new Mail("Great opportunity", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
-            AddMail(mail, Folder.inbox);
-            
+            if (name == "pablo522@o2.pl")
+            {
+                Mail mail = new Mail("Changes", "pawel.tomaszewski@gmail.com", "from@gmail.com", "There was many changes. Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("Test mail", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("Arsenal back home", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("Injury", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("new phone number", "pawel.tomaszewski@gmail.com", "from@gmail.com", "There was many changes. Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("Great opportunity", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.inbox);
 
-            mail = new Mail("More money", "pawel.tomaszewski@gmail.com", "lottery@gmail.com", "so much money to win");
-            AddMail(mail, Folder.sent);
-            mail = new Mail("interesting project", "pawel.tomaszewski@gmail.com", "lotto@gmail.com", "NOOOOOOO to Thanks for your time");
-            AddMail(mail, Folder.sent);
 
-            mail = new Mail("Lottery", "pawel.tomaszewski@gmail.com", "lottery@gmail.com", "so much money to win");
-            AddMail(mail, Folder.spam);
-            mail = new Mail("1 000 000 euro to win", "pawel.tomaszewski@gmail.com", "lotto@gmail.com", "NOOOOOOO to Thanks for your time");
-            AddMail(mail, Folder.spam);
+                mail = new Mail("More money", "pawel.tomaszewski@gmail.com", "lottery@gmail.com", "so much money to win");
+                AddMail(mail, Folder.sent);
+                mail = new Mail("interesting project", "pawel.tomaszewski@gmail.com", "lotto@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.sent);
 
-            mail = new Mail("Liverpool isn't gonna win", "pawel.tomaszewski@gmail.com", "test@gmail.com", "Message");
-            AddMail(mail, Folder.deleted);
-            mail = new Mail("delivery", "pawel.tomaszewski@gmail.com", "test@gmail.com", "not really important messages");
-            AddMail(mail, Folder.deleted);
+                mail = new Mail("Lottery", "pawel.tomaszewski@gmail.com", "lottery@gmail.com", "so much money to win");
+                AddMail(mail, Folder.spam);
+                mail = new Mail("1 000 000 euro to win", "pawel.tomaszewski@gmail.com", "lotto@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.spam);
+
+                mail = new Mail("Liverpool isn't gonna win", "pawel.tomaszewski@gmail.com", "test@gmail.com", "Message");
+                AddMail(mail, Folder.deleted);
+                mail = new Mail("delivery", "pawel.tomaszewski@gmail.com", "test@gmail.com", "not really important messages");
+                AddMail(mail, Folder.deleted);
+            }
+            if (name == "pawel.tomaszewski@gmail.com")
+            {
+                Mail mail = new Mail("New mail", "pawel.tomaszewski@gmail.com", "from@gmail.com", "There was many changes. Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("Geburstag", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("New message for you", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("no lottery for this day", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("departure", "pawel.tomaszewski@gmail.com", "from@gmail.com", "There was many changes. Thanks for your time");
+                AddMail(mail, Folder.inbox);
+                mail = new Mail("take off", "pawel.tomaszewski@gmail.com", "test@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.inbox);
+
+
+                mail = new Mail("basketball", "pawel.tomaszewski@gmail.com", "lottery@gmail.com", "so much money to win");
+                AddMail(mail, Folder.sent);
+                mail = new Mail("volayball", "pawel.tomaszewski@gmail.com", "lotto@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.sent);
+
+                mail = new Mail("Mega bank", "pawel.tomaszewski@gmail.com", "lottery@gmail.com", "so much money to win");
+                AddMail(mail, Folder.spam);
+                mail = new Mail("To sell", "pawel.tomaszewski@gmail.com", "lotto@gmail.com", "NOOOOOOO to Thanks for your time");
+                AddMail(mail, Folder.spam);
+
+                mail = new Mail("New football team", "pawel.tomaszewski@gmail.com", "test@gmail.com", "Message");
+                AddMail(mail, Folder.deleted);
+                mail = new Mail("lecture cancelled", "pawel.tomaszewski@gmail.com", "test@gmail.com", "not really important messages");
+                AddMail(mail, Folder.deleted);
+            }
 
         }
 
